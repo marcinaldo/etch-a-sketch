84489439ff422147b24fb6b198c6f678692fa3e7
marcinaldo <marcinaldo@gmail.com>
2014-04-05 14:01:31 +0200

    almost working

--- js/etch.js
+++ js/etch.js
@@ -1,5 +1,22 @@
 $(document).ready(function () {
+	addDiv(16);
 	$("div").on("mouseenter", ".square", function () {
 		$(this).addClass("coloured");
 	});
+	$("button").on("click", function () {
+		var selectNum = prompt("Please enter number for grid size");
+		$("div").removeClass("coloured");
+		addDiv(selectNum);
 	});
+	});
+
+
+function addDiv(number) {
+	var divWidth = (600 - (number * 2)) / number;
+	var divNumber = number;
+	for (var i = 1; i <= divNumber * divNumber; i++) {
+		$(".box").append('<div class="square"></div>')
+	}
+	$(".square").width(divWidth);
+	$(".square").height(divWidth);
+}
